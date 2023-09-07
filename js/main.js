4072e4a40760b7cb7801d0abc79aa42b97e0ce48
NamJiwan <129821189+NamJiwan@users.noreply.github.com>
2023-09-07 15:16:52 +0900

    fade

--- js/main.js
+++ js/main.js
@@ -27,16 +27,4 @@ $(function () {
       });
     },
   });
-
-  // $(window).scroll(function () {
-  //   if ($(this).scrollTop() > 100) {
-  //     $("#backToTop").fadeIn(500);
-  //   } else {
-  //     $("#backToTop").fadeOut("slow");
-  //   }
-  // });
-  // $("#backToTop").click(function (e) {
-  //   e.preventDefault();
-  //   $("html, body").animate({ scrollTop: 0 }, 1000);
-  // });
 });
